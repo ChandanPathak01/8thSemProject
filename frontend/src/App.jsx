00b862319d7531e0d4c2c693cac2cb7825943192
aditi <97868971+adi7201@users.noreply.github.com>
2023-06-21 18:34:22 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,46 @@
 import "./App.css";
+import React from 'react';
+import {  Routes, Route, BrowserRouter } from "react-router-dom";
+import AdminHome from './pages/AdminHome';
+import UserPage from './pages/UserPage';
+import Profile from './pages/Profile';
+import ManageUser from './pages/ManageUser';
+import RegistrationForm from './pages/RegistrationForm';
+import Login from './pages/Login';
+import FacultyHome from './pages/FacultyHome';
+import ApplyLeave from './pages/ApplyLeave';
+import LeaveHistory from './pages/LeaveHistory';
+import HodHome from './pages/HodHome';
+import FacultyRequest from './pages/FacultyRequest';
+import HodRequest from './pages/HodRequest';
+import HodApproved from './pages/HodApproved';
+import PrincipalHome from './pages/PrincipalHome';
+// import ProtectedRoute from './ProtectedRoute';
 
-import {  Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
-import AdminHome from "./pages/AdminHome";
- 
-import FacultyHome from "./pages/FacultyHome";
- 
- 
  
 
 function App() {
   return (
-     
+     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin-home" element={<AdminHome />} />
+        <Route path="/ManageUser" element={<ManageUser />} />
+        <Route path="/RegistrationForm" element={<RegistrationForm />} />
+        <Route path="/UserPage" element={<UserPage />} />
         <Route path="/faculty-home" element={<FacultyHome />} />
-        
+        <Route path="/ApplyLeave" element={<ApplyLeave />} />
+        {/* <Route path="/PeopleOnLeave" element={<PeopleOnLeave/>}/>  */}
+        <Route path='/leaveHistory' element={<LeaveHistory/>}/>
+        <Route path='/hod-home'  element={<HodHome/>}/>
+        <Route path='/principal-home'  element={<PrincipalHome/>}/>
+        <Route path='/FacultyRequest' element={<FacultyRequest/>}/>
+        <Route path='/hodRequest' element={<HodRequest/>}/>
+        <Route path='/hodApproved' element={<HodApproved/>}/>
+        <Route path='/Profile' element={<Profile/>}/>
       </Routes>
-    
+    </BrowserRouter>
   );
 }
 
