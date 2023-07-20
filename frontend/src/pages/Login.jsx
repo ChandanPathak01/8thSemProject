c0ddbef68ad841a4b58bf7cc3c48716d1d7a8df3
aditi <97868971+adi7201@users.noreply.github.com>
2023-07-20 23:21:01 +0530

    login changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,139 +1,115 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+ 
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [showFullText, setShowFullText] = useState(false);
 
   let navigate = useNavigate();
 
-  const onLogin = async () => {
+  const onLogin = async (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+
     const credentials = {
       email,
       password,
     };
 
-    await axios
-      .post("http://localhost:8000/login", credentials)
-      .then((response) => {
-        localStorage.setItem('token',response.data.token);
-        localStorage.setItem('role',response.data.role);
-        if (response.data.role=== "Admin") {
-          navigate("/admin-home");
-        } else if (response.data.role === "Faculty") {
-          navigate("/faculty-home");
-        }
-        else if (response.data.role === "HOD") {
-          navigate("/hod-home");
-        }
-        else if (response.data.role === "Principal") {
-          navigate("/principal-home");
-        }
-      })
-      .catch((error) => {
-        alert(error.response.data.message);
-      });
+    try {
+      const response = await axios.post("http://localhost:8000/login", credentials);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("role", response.data.role);
+      if (response.data.role === "Admin") {
+        navigate("/admin-home");
+      } else if (response.data.role === "Faculty") {
+        navigate("/faculty-home");
+      } else if (response.data.role === "HOD") {
+        navigate("/hod-home");
+      } else if (response.data.role === "Principal") {
+        navigate("/principal-home");
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
   };
+
+  // const toggleReadMore = () => {
+  //   setShowFullText((prev) => !prev);
+  // };
 
   return (
     <>
-      <section className="vh-100">
-        <div className="container-fluid h-custom">
-          <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-            {/* <div className="col-md-9 col-lg-6 col-xl-5">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                className="img-fluid"
-                alt=""
+      {/* <div className="login-page"> */}
+      {/* <div className="background-image"  >
+          <h1>Leave Management System</h1>
+          {showFullText ? (
+           <>  
+            <p>
+              In today's fast-paced world, managing leaves efficiently 
+              is crucial to ensure seamless operations and maintain a harmonious work environment. This project aims to provide
+              a user-friendly and responsive solution for faculty, Heads of Departments (HODs), and the Principal, empowering 
+              them to manage leave applications, track leave history, and monitor the availability of staff on leave.
+              The Leave Management System is a web-based application designed to revolutionize the leave application 
+              and approval process within educational institutions. In today's fast-paced world, managing leaves efficiently 
+              is crucial to ensure seamless operations and maintain a harmonious work environment...
+            </p>
+            <span className="read-more" onClick={toggleReadMore}>
+              Read Less
+            </span>
+          </>
+          ) : (
+            <>
+             
+              <p>
+                The Leave Management System is a web-based application designed to revolutionize the leave application 
+                and approval process within educational institutions.  
+              </p>
+              <span className="read-more" onClick={toggleReadMore}>
+                Read More
+              </span>
+            </>
+             
+             
+          )} */}
+        {/* </div> */}
+
+        
+        <div className="login-form-container">
+          <div className="login-form">
+            <h2>Welcome Back to Login Page</h2>
+            <form onSubmit={onLogin}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                placeholder="Enter your Email Id"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
-            </div> */}
-            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form>
-                <div className="form-outline mb-4">
-                  <p  style={{ fontWeight: 'bold', fontSize: '20px', color: 'black' }}>Email :</p>
-                  {/* EMAIL INPUT */}
-                  <input
-                    type="email"
-                    id="form3Example3"
-                    className="form-control form-control-lg custom-outline"
-                    placeholder="Enter a valid email address"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
 
-                <div className="form-outline mb-3">
-                  <p  style={{ fontWeight: 'bold', fontSize: '20px', color: 'black' }}>Password : </p>
-                  <input
-                    type="password"
-                    id="form3Example4"
-                    className="form-control form-control-lg custom-outline"
-                    placeholder="Enter password"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                  />
-                </div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Enter your Password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
 
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="form-check mb-0">
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      value=""
-                      id="form2Example3"
-                    />
-                    <label className="form-check-label" for="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="/" className="text-body">
-                    Forgot password?
-                  </a>
-                </div>
-
-                <div className="text-center text-lg-center mt-4 pt-2">
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-lg"
-                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                    onClick={onLogin}
-                  >
-                    Login
-                  </button>
-                   
-                </div>
-              </form>
-            </div>
+              <button type="submit">Login</button>
+            </form>
           </div>
         </div>
-        <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-7 bg-primary">
-          <div className="text-white mb-3 mb-md-0">
-            Copyright © 2023. All rights reserved to MIT Muzaffarpur .
-          </div>
-
-          {/* <div>
-            <a href="/" className="text-white me-4">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="/" className="text-white me-4">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="/" className="text-white me-4">
-              <i className="fab fa-google"></i>
-            </a>
-            <a href="/" className="text-white">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div> */}
-        </div>
-      </section>
+       
     </>
   );
 };
 
 export default Login;
-
-
- 
