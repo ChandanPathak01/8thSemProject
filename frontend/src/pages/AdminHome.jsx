bbd5edb16d4eb33ccad78f4e6bd75662eafdf664
aditi <97868971+adi7201@users.noreply.github.com>
2023-08-09 17:45:52 +0530

    hodhome

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
- 
 import { Link} from "react-router-dom";
 import axios from 'axios';
 import Navbar from '../Layout/Navbar';
 
 function AdminHome() {
-   
   const [data, setData] = useState([]);
   const token = localStorage.getItem("token");
   useEffect(() => {
--- frontend/src/pages/HodHome.jsx
+++ frontend/src/pages/HodHome.jsx
@@ -1,80 +1,52 @@
 import React from "react";
-<<<<<<< Updated upstream
-import { Link } from "react-router-dom";
 import PeopleLeave from "./PeopleLeave";
 import NavbarHod from "../Layout/NavbarHod";
+import { Link} from "react-router-dom";
  
 
-function HodHome() {
-=======
-import { useState, useEffect    } from "react";
-
-import { Link, useNavigate } from "react-router-dom";
-import { FaBars } from "react-icons/fa"
-import { ImCross } from "react-icons/im"
-import axios from "axios";
- 
 
 function HodHome() {
      
-    const navigate = useNavigate();
+     
   
     
-    const handleLogout = async() => {
-      const token = localStorage.getItem("token");
-      axios
-      .post(
-        `http://localhost:8000/logout`,
-        { },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        localStorage.removeItem('token');
+    // const handleLogout = async() => {
+    //   const token = localStorage.getItem("token");
+    //   axios
+    //   .post(
+    //     `http://localhost:8000/logout`,
+    //     { },
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Authorization: `Bearer ${token}`,
+    //       },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     localStorage.removeItem('token');
 
-      navigate('/');
-      })
-      .catch((error) => {
-        console.error("Error in logout:", error);
-      });
+    //   navigate('/');
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error in logout:", error);
+    //   });
       
-    }
-    const [showList, setShowList] = useState(false);
-    const [leaveData, setLeaveData] = useState([]);
-    const [selectedDate, setSelectedDate] = useState('');
-    const [Mobile, setMobile] = useState(false)
-
+    // }
      
-    const toggleList = () => {
-      setShowList(!showList);
-    };
+    // const handleLogout = () => {
+    //   localStorage.removeItem('token');
+    //   navigate('/Login');
+    // }
+     
+     
     
   
      
   
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          let url = "http://localhost:8000/leaveList";
-          if (selectedDate) {
-            url += `?date=${selectedDate}`;
-          }
-          const response = await fetch(url);
-          const data = await response.json();
-          setLeaveData(data.pplList);
-        } catch (error) {
-          console.error("Error fetching leave data:", error);
-        }
-      };
-  
-      fetchData();
-    }, [selectedDate]);
+   
 
->>>>>>> Stashed changes
+ 
     return (
       <div>
          <NavbarHod/>
